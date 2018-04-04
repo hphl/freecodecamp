30a4b3c09124229958e821be028c1bac6b65c5ef
“hphl” <hp_hamel@hotmail.com>
2018-04-03 23:16:42 -0400

    add the calculator project including the layout

---
+++ calculator/javascript.js
@@ -0,0 +1,15 @@
+(function() {
+    function Calculator() {
+        this.id = id;
+    }
+    
+})();
+
+(function(){
+    var allBtns = document.getElementsByTagName('button');
+    for (let index = 0; index < allBtns.length; index++) {
+        allBtns[index].addEventListener("click", function(event){
+            
+        });
+    }
+})();
